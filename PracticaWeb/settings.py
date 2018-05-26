d12e17906f597f786dac39c0535a4658cad38554
Soulk97 <david.martin2897@gmail.com>
2018-05-27 00:12:09 +0200

    xxdddd4

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -133,6 +133,7 @@ django_heroku.settings(locals())
 #Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
